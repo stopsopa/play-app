0c7ab03d5c40105c1b0e1b9a060c36a49852b801
Szymon Dzialowski <szymon.dzialowski@gmail.com>
2026-08-16 17:58:07 +0100

    first commit

---
+++ sanemp3/sanemp3/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  sanemp3
+//
+//  Created by szdz on 16/08/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ sanemp3/sanemp3/sanemp3App.swift
@@ -0,0 +1,17 @@
+//
+//  sanemp3App.swift
+//  sanemp3
+//
+//  Created by szdz on 16/08/2026.
+//
+
+import SwiftUI
+
+@main
+struct sanemp3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
